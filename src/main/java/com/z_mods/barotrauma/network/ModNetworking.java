8a64953b666166f9469341f310da6be55bed0282
Denis18UKS <148848338+Denis18UKS@users.noreply.github.com>
2026-08-04 01:06:57 +0500

    fix(panel): update src/main/java/com/z_mods/barotrauma/network/ModNetworking.java

--- src/main/java/com/z_mods/barotrauma/network/ModNetworking.java
+++ src/main/java/com/z_mods/barotrauma/network/ModNetworking.java
@@ -6,7 +6,7 @@ import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 
 public final class ModNetworking {
-    private static final String PROTOCOL_VERSION = "3";
+    private static final String PROTOCOL_VERSION = "4";
 
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
             new ResourceLocation(Barotrauma.MOD_ID, "main"),
